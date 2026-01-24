6ed02960233329ae70f4889cd1529c0fe8b43351
DaliTesfaye <112868614+DaliTesfaye@users.noreply.github.com>
2026-01-24 16:18:05 +0100

    feat : stack options

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,10 +4,10 @@ import { Slot, Stack } from "expo-router";
 
 const Rootlayout = () => {
   return (
-      <Stack>
+      <Stack screenOptions={{ headerStyle: { backgroundColor: '#ddd' }, headerTintColor: '#333' }}>
         <Stack.Screen name="index" options={{title : "Home"}} />
         <Stack.Screen name="about" options={{title : "About"}} />
-        <Stack.Screen name="contact" options={{title : "Contact"}} />
+        <Stack.Screen name="contact" options={{title : "Contact" , headerShown : false}} />
       </Stack>
   );
 };
